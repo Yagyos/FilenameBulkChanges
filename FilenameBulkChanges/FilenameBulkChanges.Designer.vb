483189169fa2b9a59cb7e55ef68fd35b0d37bc38
Yagyos <Yagyos@users.noreply.github.com>
2019-10-18 15:23:00 +0900

    ひとまず完了

--- FilenameBulkChanges/FilenameBulkChanges.Designer.vb
+++ FilenameBulkChanges/FilenameBulkChanges.Designer.vb
@@ -22,41 +22,158 @@ Partial Class FilenameBulkChanges
     'コード エディターを使って変更しないでください。
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BtnSelect = New System.Windows.Forms.Button()
+        Me.FilenameList = New System.Windows.Forms.ListBox()
+        Me.FolderPathName = New System.Windows.Forms.TextBox()
+        Me.TxtFormat = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TxtStartNo = New System.Windows.Forms.TextBox()
+        Me.BtnRenum = New System.Windows.Forms.Button()
+        Me.BtnReacquire = New System.Windows.Forms.Button()
+        Me.TxtStepNo = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnGo = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
-        'Button1
+        'BtnSelect
         '
-        Me.Button1.Location = New System.Drawing.Point(12, 12)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(141, 24)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "ChangeFolder"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BtnSelect.Location = New System.Drawing.Point(12, 12)
+        Me.BtnSelect.Name = "BtnSelect"
+        Me.BtnSelect.Size = New System.Drawing.Size(66, 19)
+        Me.BtnSelect.TabIndex = 3
+        Me.BtnSelect.Text = "Select"
+        Me.BtnSelect.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'FilenameList
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 12
-        Me.ListBox1.Location = New System.Drawing.Point(12, 42)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(196, 196)
-        Me.ListBox1.TabIndex = 2
+        Me.FilenameList.FormattingEnabled = True
+        Me.FilenameList.HorizontalScrollbar = True
+        Me.FilenameList.ItemHeight = 12
+        Me.FilenameList.Location = New System.Drawing.Point(12, 42)
+        Me.FilenameList.Name = "FilenameList"
+        Me.FilenameList.Size = New System.Drawing.Size(196, 196)
+        Me.FilenameList.TabIndex = 2
+        '
+        'FolderPathName
+        '
+        Me.FolderPathName.Location = New System.Drawing.Point(84, 12)
+        Me.FolderPathName.Name = "FolderPathName"
+        Me.FolderPathName.Size = New System.Drawing.Size(196, 19)
+        Me.FolderPathName.TabIndex = 4
+        '
+        'TxtFormat
+        '
+        Me.TxtFormat.Location = New System.Drawing.Point(261, 42)
+        Me.TxtFormat.Name = "TxtFormat"
+        Me.TxtFormat.Size = New System.Drawing.Size(137, 19)
+        Me.TxtFormat.TabIndex = 5
+        Me.TxtFormat.Text = """Page""0000"".jpg"""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(214, 45)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(41, 12)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Format"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(214, 70)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 12)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "StartNo"
+        '
+        'TxtStartNo
+        '
+        Me.TxtStartNo.Location = New System.Drawing.Point(261, 67)
+        Me.TxtStartNo.Name = "TxtStartNo"
+        Me.TxtStartNo.Size = New System.Drawing.Size(137, 19)
+        Me.TxtStartNo.TabIndex = 8
+        Me.TxtStartNo.Text = "1"
+        '
+        'BtnRenum
+        '
+        Me.BtnRenum.Location = New System.Drawing.Point(261, 117)
+        Me.BtnRenum.Name = "BtnRenum"
+        Me.BtnRenum.Size = New System.Drawing.Size(66, 19)
+        Me.BtnRenum.TabIndex = 9
+        Me.BtnRenum.Text = "Renum"
+        Me.BtnRenum.UseVisualStyleBackColor = True
+        '
+        'BtnReacquire
+        '
+        Me.BtnReacquire.Location = New System.Drawing.Point(286, 12)
+        Me.BtnReacquire.Name = "BtnReacquire"
+        Me.BtnReacquire.Size = New System.Drawing.Size(66, 19)
+        Me.BtnReacquire.TabIndex = 10
+        Me.BtnReacquire.Text = "Reacquire"
+        Me.BtnReacquire.UseVisualStyleBackColor = True
+        '
+        'TxtStepNo
+        '
+        Me.TxtStepNo.Location = New System.Drawing.Point(261, 92)
+        Me.TxtStepNo.Name = "TxtStepNo"
+        Me.TxtStepNo.Size = New System.Drawing.Size(137, 19)
+        Me.TxtStepNo.TabIndex = 12
+        Me.TxtStepNo.Text = "1"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(214, 95)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 12)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "StepNo"
+        '
+        'BtnGo
+        '
+        Me.BtnGo.Location = New System.Drawing.Point(216, 191)
+        Me.BtnGo.Name = "BtnGo"
+        Me.BtnGo.Size = New System.Drawing.Size(182, 47)
+        Me.BtnGo.TabIndex = 13
+        Me.BtnGo.Text = "Go"
+        Me.BtnGo.UseVisualStyleBackColor = True
         '
         'FilenameBulkChanges
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(421, 279)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BtnGo)
+        Me.Controls.Add(Me.TxtStepNo)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.BtnReacquire)
+        Me.Controls.Add(Me.BtnRenum)
+        Me.Controls.Add(Me.TxtStartNo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtFormat)
+        Me.Controls.Add(Me.FolderPathName)
+        Me.Controls.Add(Me.BtnSelect)
+        Me.Controls.Add(Me.FilenameList)
         Me.Name = "FilenameBulkChanges"
         Me.Text = "FilenameBulkChanges"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents BtnSelect As System.Windows.Forms.Button
+    Friend WithEvents FilenameList As System.Windows.Forms.ListBox
+    Friend WithEvents FolderPathName As System.Windows.Forms.TextBox
+    Friend WithEvents TxtFormat As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TxtStartNo As System.Windows.Forms.TextBox
+    Friend WithEvents BtnRenum As System.Windows.Forms.Button
+    Friend WithEvents BtnReacquire As System.Windows.Forms.Button
+    Friend WithEvents TxtStepNo As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents BtnGo As System.Windows.Forms.Button
 
 End Class
--- FilenameBulkChanges/FilenameBulkChanges.vb
+++ FilenameBulkChanges/FilenameBulkChanges.vb
@@ -1,3 +1,223 @@
 ﻿Public Class FilenameBulkChanges
 
+    Public Structure FBCList_Str
+        Dim SrcFileName As String
+        Dim DstFileName As String
+    End Structure
+    Public DatFBCList() As FBCList_Str
+    Public DatFBCPathNm As String
+
+    'フォルダ選択
+    '[Select]実行
+    Private Sub BtnSelect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnSelect.Click
+        Call BtnSelect_Exec(DatFBCList)
+    End Sub
+    Public Sub BtnSelect_Exec(ByRef WrkFBCList() As FBCList_Str)
+
+        'フォルダ選択
+        If BtnSelect_Core() = True Then
+            'フォルダが選択された場合
+
+            '[BtnReacquire]実行
+            Call BtnReacquire_Exec(WrkFBCList)
+
+        End If
+    End Sub
+    Public Function BtnSelect_Core() As Boolean
+
+        Dim WrkFBD As New FolderBrowserDialog
+        With WrkFBD
+
+            .RootFolder = Environment.SpecialFolder.Desktop 'ルートフォルダ
+            .SelectedPath = FolderNameCheck(FolderPathName.Text) '初期フォルダ
+            .ShowNewFolderButton = False '新しいフォルダの作成
+
+            'ダイアログ表示
+            If .ShowDialog(Me) = DialogResult.OK Then
+
+                FolderPathName.Text = .SelectedPath
+
+                Return True
+
+            End If
+
+        End With
+
+        Return False
+    End Function
+
+    'フォルダ名をきれいにする
+    Public Function FolderNameCheck(ByVal WrkStr As String) As String
+
+        If Strings.Left(WrkStr, 1) = """" And Strings.Right(WrkStr, 1) = """" Then
+            '前後に["]が付いている場合
+
+            '前後の["]を除ける
+            WrkStr = Microsoft.VisualBasic.Strings.Mid(WrkStr, 2, WrkStr.Length - 2)
+
+        End If
+
+        If Strings.Right(WrkStr, 1) = "\" Then
+            '文字列の最後が[\]の場合
+
+            '[\]を除ける
+            WrkStr = Microsoft.VisualBasic.Strings.Left(WrkStr, WrkStr.Length - 1)
+
+        End If
+
+        Return WrkStr
+    End Function
+
+    'ファイル名を検索
+    '[BtnReacquire]実行
+    Private Sub BtnReacquire_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnReacquire.Click
+        Call BtnReacquire_Exec(DatFBCList)
+    End Sub
+    Public Sub BtnReacquire_Exec(ByRef WrkFBCList() As FBCList_Str)
+
+        'ファイル名を検索
+        Call BtnReacquire_Core(WrkFBCList)
+
+        '[BtnRenum]実行
+        Call BtnRenum_Exec(WrkFBCList)
+
+    End Sub
+    Public Sub BtnReacquire_Core(ByRef WrkFBCList() As FBCList_Str)
+
+        DatFBCPathNm = FolderNameCheck(FolderPathName.Text)
+
+        On Error Resume Next
+        Dim WrkFileList As String() = System.IO.Directory.GetFiles(DatFBCPathNm)
+        On Error GoTo 0
+
+        Erase WrkFBCList
+
+        If Not WrkFileList Is Nothing Then
+
+            'ファイル名部分のみに変換
+            For Each WrkStr As String In WrkFileList
+
+                If WrkFBCList Is Nothing Then
+                    ReDim WrkFBCList(0)
+                Else
+                    ReDim Preserve WrkFBCList(WrkFBCList.Count)
+                End If
+
+                WrkFBCList(WrkFBCList.Count - 1).SrcFileName = System.IO.Path.GetFileName(WrkStr)
+
+            Next
+        End If
+
+    End Sub
+
+    '新ファイル名生成
+    '[BtnRenum]実行
+    Private Sub BtnRenum_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnRenum.Click
+        Call BtnRenum_Exec(DatFBCList)
+    End Sub
+    Public Sub BtnRenum_Exec(ByRef WrkFBCList() As FBCList_Str)
+
+        '新ファイル名生成
+        Call BtnRenum_Core(WrkFBCList)
+
+        'リスト再表示
+        Call ListView(WrkFBCList)
+
+    End Sub
+    Public Sub BtnRenum_Core(ByRef WrkFBCList() As FBCList_Str)
+
+        Dim StartNo As Integer
+        StartNo = CInt(TxtStartNo.Text)
+
+        If Not WrkFBCList Is Nothing Then
+
+            For WrkIdx As Integer = 0 To WrkFBCList.Count - 1
+
+                WrkFBCList(WrkIdx).DstFileName = StartNo.ToString(TxtFormat.Text)
+
+                StartNo = StartNo + CInt(TxtStepNo.Text)
+            Next
+
+        End If
+
+    End Sub
+
+    'リスト再表示
+    Public Sub ListView(ByVal WrkFBCList() As FBCList_Str)
+
+        FilenameList.Items.Clear()
+
+        If Not WrkFBCList Is Nothing Then
+
+            For WrkIdx As Integer = 0 To WrkFBCList.Count - 1
+                FilenameList.Items.Add(WrkFBCList(WrkIdx).SrcFileName & " → " & WrkFBCList(WrkIdx).DstFileName)
+            Next
+
+        End If
+
+    End Sub
+
+    '[Go]実行
+    Private Sub BtnGo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnGo.Click
+        Call BtnGo_Exec(DatFBCList)
+    End Sub
+    Public Sub BtnGo_Exec(ByVal WrkFBCList() As FBCList_Str)
+
+        If WrkFBCList Is Nothing Then
+            Call MessageBox.Show("対象データがありません。" & vbCrLf & "There is no target data.", "確認 Check", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        If MessageBox.Show("ファイル名を変更します。" & vbCrLf & "Change the file name.", "確認 Confirmation", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) <> DialogResult.OK Then
+            Exit Sub
+        End If
+
+        Dim ErrMsg As String = ""
+
+        If BtnGo_Core(WrkFBCList, ErrMsg) = True Then
+            Call MessageBox.Show("完了しました" & vbCrLf & "Completed.", "完了 Complete", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        Else
+            If MessageBox.Show("完了しましたが、エラーが発生しています。" & vbCrLf & "Completed with errors." & vbCrLf & vbCrLf _
+                               & "エラー内容をクリップボードに保存しますか？" & vbCrLf & "Do you want to save the error contents to the clipboard?" _
+                               , "完了 Complete", MessageBoxButtons.OKCancel, MessageBoxIcon.Error) = DialogResult.OK Then
+
+                Do
+
+                    'エラー内容をクリップボードに保存
+                    Clipboard.SetText(ErrMsg)
+
+                Loop While Windows.Forms.DialogResult.No = MessageBox.Show( _
+                            "クリップボードに保存しました。" & vbCrLf & _
+                            "Saved to clipboard." & vbCrLf & _
+                            vbCrLf & _
+                            "[いいえ]で再度保存を行います。" & vbCrLf & _
+                            "Save again with [No]." & vbCrLf & _
+                            "", "保存しました Saved", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+            End If
+        End If
+
+    End Sub
+    Public Function BtnGo_Core(ByVal WrkFBCList() As FBCList_Str, ByRef ErrMsg As String) As Boolean
+
+        Dim RetCode As Boolean = True
+
+        For WrkIdx As Integer = 0 To WrkFBCList.Count - 1
+
+            Try
+
+                'ファイル名の変更
+                System.IO.File.Move(DatFBCPathNm & "\" & WrkFBCList(WrkIdx).SrcFileName _
+                                  , DatFBCPathNm & "\" & WrkFBCList(WrkIdx).DstFileName)
+
+            Catch ex As Exception
+                ErrMsg = ErrMsg & ex.Message & vbCrLf
+                RetCode = False
+            End Try
+
+        Next
+
+        Return RetCode
+    End Function
+
 End Class
